856f0305dac7f2c0d95ca7635f77c827218d9bcf
Prince-24-T <pt023150@gmail.com>
2026-04-23 23:32:44 +0530

    complex query

--- join.sql
+++ join.sql
@@ -143,3 +143,40 @@ group by role
  SELECT sum(years_employed) as totalYear , building
 from Employees
 group by building
+
+-- Table: employees
+-- role	name	building	years_employed
+-- Engineer	Becky A.	1e	4
+-- Engineer	Dan B.	1e	2
+-- Engineer	Sharon F.	1e	6
+-- Engineer	Dan M.	1e	4
+-- Engineer	Malcom S.	1e	1
+-- Artist	Tylar S.	2w	2
+-- Artist	Sherman D.	2w	8
+-- Artist	Jakob J.	2w	6
+-- Artist	Lillia A.	2w	7
+-- Artist	Brandon J.	2w	7
+-- Manager	Scott K.	1e	9
+-- Manager	Shirlee M.	1e	3
+-- Manager	Daria O.	2w	6
+
+Questions:: 1 --> Find the number of Artists in the studio (without a HAVING clause) 
+
+
+Select count(role) as numberOFartict 
+from employees
+where role = "Artist"
+
+
+Questions::2--> Find the number of Employees of each role in the studio
+
+Select count(*) as numberOFemplyee , role 
+from employees
+group by role
+
+Questions:: 3==> Find the total number of years employed by all Engineers 
+
+Select sum(years_employed) as totalYear , role
+from employees
+ group by role
+ having role ="Engineer"
