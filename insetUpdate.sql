f83fb80130e830d324b6eecbdb92d8aca5ef5ac2
Prince-24-T <pt023150@gmail.com>
2026-04-25 00:25:28 +0530

    query

--- insetUpdate.sql
+++ insetUpdate.sql
@@ -56,3 +56,17 @@ Question::2==>Andrew Stanton has also left the studio, so please remove all movi
 delete from movies
 where director="Andrew Stanton"
 
+
+
+Question::3=>Add a column named Aspect_ratio with a FLOAT data type to store the aspect-ratio each movie was released in.
+
+
+ALTER table movies
+Add Aspect_ration float; 
+
+Question::4 => ADDAdd another column named Language with a TEXT data type to store the language that the movie was released in. Ensure that the default for this language is English. 
+
+ALTER table movies
+Add Language text 
+default "English"; 
+
